3271446106c299fc162eae21e252bbcda33b1c41
YordanovaT <tanyaiyord@gmail.com>
2023-11-29 17:23:46 +0200

    Changes in .h file and class Employee methods.

--- employees.cpp
+++ employees.cpp
@@ -1,29 +1,59 @@
 #include "employees.h"
 
-Employee::Employee()
+Employee::~Employee()
 {
-    ID=0;
-    first_name="Not set";
-    last_name= "Not set";
-    Age=0;
-    month_salary=0;
     
 }
 
-Employee::Employee(int id, std::string f_name, std::string l_name, std::string phone_num, int age, float month_sal,  std::string company_name,  std::string department)
-:Company(company_name, department)
+Employee::Employee()
 {
+    int id, age; float month_sal;
+    std::string f_name, l_name, phone_num, dep;
+
+    std::cout<<"Enter employee's  ID: ";
+    std::cin>>id;
+    std::cin.ignore();
+
+    std::cout<<"\nEnter employee's first name: ";
+    std::cin>>f_name;
+    std::cin.ignore();
+
+    std::cout<<"\nEnter employee's last name: ";
+    std::cin>>l_name;
+    std::cin.ignore();
+
+    std::cout<<"\nEnter employee's phone: ";
+    std::cin>>phone_num;
+    std::cin.ignore();
+
+    std::cout<<"\nEnter employee's age: ";
+    std::cin>>age;
+    std::cin.ignore();
+    
+    std::cout<<"\nEnter employee's month salary: ";
+    std::cin>>month_sal;
+    std::cin.ignore();
+
+    std::cout<<"\nEnter employee's department: ";
+    std::cin>>dep;
+    std::cin.ignore();
+
     ID=id;
     first_name = f_name;
     last_name= l_name;
     phone=phone_num;
     Age=age;
-    email=first_name + "." + last_name + "@mail.com";
     month_salary=month_sal;
+    department=dep;
+
+    email=first_name + "." + last_name + "@mail.com";
+    year_income=month_salary*12;
+    bonus_percentage=0;
 }
+
 float Employee::GetIncome()
 {
-    year_income=month_salary*12;
+    //year_income=month_salary*12;
     return year_income;
 }
 
@@ -37,25 +67,24 @@ void Employee::calculate_year_income()
 {
     float sum=GetIncome();
     year_income=sum;
-    std::cout<<"Employee ID: "<< ID<<"\tYear income: "<< year_income <<"\tDepartment: "<<comp_department<<std::endl;
+    std::cout<<"Employee ID: "<< ID<<"\tYear income: "<< year_income <<"\tDepartment: "<<department<<std::endl;
 }
 
 void Employee::output()
 {
-    std::cout<<"Employee ID: "<<ID<<"\tCompany Name: " << company_name<<"\tDepartment: "<<comp_department<<std::endl;
+    std::cout<<"Employee ID: "<<ID<<"\tDepartment: "<<department<<std::endl;
     std::cout<<"Name: "<< first_name<< " "<<last_name << "\tAge: "<<Age<< "\nMonth salary: "<<month_salary<<std::endl;
 }
 
 void Employee::show_richest()
 {
     float max=30000;
-    float rich = GetIncome();
     
-    if(rich>=max)
+    if(year_income>=max)
     {
         std::cout<<"--------------------------------------- Employees with income above 30K --------------------------------------- \n";
-        std::cout<<"Employee ID: "<<ID<<"\tCompany Name: " << company_name<<std::endl;
-        std::cout<<"Name: "<< first_name<< " "<<last_name <<"\tDepartment: "<<comp_department<<"\tYear income: "<< rich<<std::endl;
+        std::cout<<"Employee ID: "<<ID<<"Name: "<< first_name<< " "<<last_name;
+        std::cout<<"\tDepartment: "<<department<<"\tYear income: "<< year_income<<std::endl;
          std::cout<<"------------------------------------------------------------------------------------------------------------- \n\n";
 
     }
@@ -65,7 +94,8 @@ void Employee::show_richest()
 void Employee::ask_for_bonus(float percent)
 {
     float pay=GetIncome();
-    float bonus= pay*(1+percent);
+    bonus_percentage=percent;
+    float bonus= pay*(1+bonus_percentage);
     year_income=bonus;
     std::cout<<"Year income after bonus: "<<year_income<<std::endl;
 }
